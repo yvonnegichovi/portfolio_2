c636a64fcd0211801c92992cdf25367609bb0889
yvonnegichovi <yvonnegichovi@gmail.com>
2025-04-14 23:19:01 +0300

    feat: Home page picture and editted the Navbar further

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import router
 import '../styles/Home.css'; // Import the CSS file
+import profileImage from '../assets/profile.png';
 
 function Home () {
   return (
 	  <>
 	  <h1>Welcome to My Portfolio</h1>
           <p>Showcasing my skills, projects, and contact info.</p>
-	  <section id="home"></section>
+	  <section id="home">
+	  <img src={profileImage} alt="Profile" className="profile-img" />
+	  </section>
 	  <section id="contact">
 	  <h2>Contact Me</h2>
 	  <p>Email: yvonnegichovi@gmail.com</p>
